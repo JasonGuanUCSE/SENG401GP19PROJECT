cdb7fc4f642f636ad72d3ddf389b5853d5fda22a
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-03-17 16:35:00 -0600

    Revert "Merge branch 'login'"
    This reverts commit b8701b453c64fa72300c90df194aef825a3883c5, reversing
    changes made to e4c9989a9dbbbfd8ea00a7c4d092b859c110afe5.
    
    Need to edit

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -1,14 +1,10 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { GoogleOAuthProvider } from "@react-oauth/google";
 import App from "./App";
-import LoginSignup from "./Components/LoginSignup/LoginSignup";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <GoogleOAuthProvider clientId="344567969650-424nh7e03gtv01cmu61htq2gcar93k46.apps.googleusercontent.com">
-    <React.StrictMode>
-      <LoginSignup />
-    </React.StrictMode>
-  </GoogleOAuthProvider>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
 );
