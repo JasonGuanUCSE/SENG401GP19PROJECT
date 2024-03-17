59d07553d913bc0e96e75e1557530343f5e23550
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-03-17 16:05:43 -0600

    test

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -7,7 +7,6 @@ import LoginSignup from "./Components/LoginSignup/LoginSignup";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <GoogleOAuthProvider clientId="344567969650-424nh7e03gtv01cmu61htq2gcar93k46.apps.googleusercontent.com">
-    // TODO: Combine the LoginSignup and App components into a single component
     <React.StrictMode>
       <LoginSignup />
     </React.StrictMode>
