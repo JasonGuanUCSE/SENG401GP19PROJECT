4cef876e6798515615755ff5ffec95e0a968c15a
Zuriel Abasola <zuriel123.abasola@gmail.com>
2024-03-19 18:48:05 -0600

    Changed signin

--- my-app/src/Components/LoginSignup.jsx
+++ my-app/src/Components/LoginSignup.jsx
@@ -18,8 +18,9 @@ const LoginSignup = ({ handleLoginSuccess }) => {
       </div>
       <div className="signup-container">
         <div className="header">
-          <div className="text">Log in</div>
-          <div className="underline"></div>
+          <div className="text">
+            <h1>Welcome to Jstacart</h1>
+          </div>
         </div>
         <div className="google-button">
           <GoogleLogin
@@ -30,27 +31,6 @@ const LoginSignup = ({ handleLoginSuccess }) => {
             cookiePolicy={"single_host_origin"}
             flow="auth-code"
           />
-        </div>
-        <div className="seperator">
-          <div className="or-text">
-            <span className="line"></span>
-            or
-            <span className="line"></span>
-          </div>
-        </div>
-        <div className="inputs">
-          <div className="input">
-            <input type="text" placeholder="Email" />
-          </div>
-          <div className="input">
-            <input type="password" placeholder="Password" />
-          </div>
-        </div>
-        <div className="forgot-password">
-          Forgot your password? <span>Click here!</span>
-        </div>
-        <div className="submit-container">
-          <div className="submit">Login</div>
         </div>
       </div>
     </div>
