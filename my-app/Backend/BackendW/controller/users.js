910692ba25844b0a23663ba0148b53fb5bb0ec1e
JasonGuanUCSE <113049965+JasonGuanUCSE@users.noreply.github.com>
2024-03-25 15:42:14 -0600

    updated user controller

--- my-app/Backend/BackendW/controller/users.js
+++ my-app/Backend/BackendW/controller/users.js
@@ -75,8 +75,8 @@ const addUser = async (req, res) => {
         const newUser = await Users.create(req.body)
         res.status(201).json(newUser)
 
-        // //Update the Read database
-        // await updateReadDB(newUser);
+        // //update the Read database
+        await updateReadDB(req.body, 'user',  'POST');
 
     } catch (err) {
         res.status(400).json({ message: err.message })
@@ -123,8 +123,8 @@ const updateUser = async (req, res) => {
             const user = await Users.updateOne({ email: req.params.email }, req.body)
             res.status(200).json(result);
         }
-        // //update the Read database
-        await updateReadDB(req.body, 'user',  'POST');
+        // // //update the Read database
+        // await updateReadDB(req.body, 'user',  'PATCH');
     }
     catch (err) {
         res.status(500).json({ message: err.message })
