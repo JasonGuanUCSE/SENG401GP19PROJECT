dda0b2faf00ed9ff5ad5e6af8746b01194d0856a
JasonGuanUCSE <113049965+JasonGuanUCSE@users.noreply.github.com>
2024-03-26 16:09:57 -0600

    fixed the get order

--- my-app/Backend/BackendW/model/orderModel.js
+++ my-app/Backend/BackendW/model/orderModel.js
@@ -3,6 +3,10 @@ const mongoose = require('../node_modules/mongoose');
 const Schema = mongoose.Schema
 
 const OrderSchema = new Schema({
+    ordeerID:{
+      type: String,
+      required: true
+    },
     customerEmail: {
       type: String,
       required: true
