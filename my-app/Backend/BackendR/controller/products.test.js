a9f92ff706c8c718e55a8de68a52c97086d608c6
Hiu Sum Yuen <hiusum.yuen@ucalgary.ca>
2024-03-26 16:44:20 -0600

    feat: <products.test.js> implement getAllProducts response test.

--- my-app/Backend/BackendR/controller/products.test.js
+++ my-app/Backend/BackendR/controller/products.test.js
@@ -10,8 +10,15 @@ afterEach(() => {
 describe("Product controller tests", () => {
     timeout = 10000; // timeout = 10 seconds
     it("should return all products", async () => {
-    // Mock getAllProducts function
-    // Expects 200 status code
+        // Mock getAllProducts function
+        const req = {};
+        const res = {
+            status: jest.fn().mockReturnThis(),
+            json: jest.fn()
+        };
+        await products.getAllProducts(req, res);
+        // Expects 200 status code
+        expect(res.status).toHaveBeenCalledWith(200);
     }, timeout);
 
     it("should return one product", async () => {
